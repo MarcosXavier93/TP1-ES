6db024dbcb2ae0f0f8e55330dc4b7861927a57ba
Marcos Junio <innformarcos@gmail.com>
2020-03-15 01:15:33 -0300

    Atualizando os generates do projetos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,24 @@
+import { CadastroTreinoComponent } from './cadastro-treino/cadastro-treino.component';
+import { VerificaAptidaoComponent } from './verifica-aptidao/verifica-aptidao.component';
+import { RealizaExameAptidaoComponent } from './realiza-exame-aptidao/realiza-exame-aptidao.component';
+import { MatriculaComponent } from './matricula/matricula.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: 'Matricula', component: MatriculaComponent },
+  { path: 'Realiza_Exame_Aptidao', component: RealizaExameAptidaoComponent },
+  { path: 'Verifica_Aptidao', component: VerificaAptidaoComponent },
+  { path: 'Cadastro_Treino', component: CadastroTreinoComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  
+
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
 import { LoginComponent } from './login/login.component';
 import { MatriculaComponent } from './matricula/matricula.component';
 import { VerificaAptidaoComponent } from './verifica-aptidao/verifica-aptidao.component';
-import { CadastroTreinoComponent } from './cadastro-treino/cadastro-treino.component';
 import { RealizaExameAptidaoComponent } from './realiza-exame-aptidao/realiza-exame-aptidao.component';
+import { CadastroTreinoComponent } from './cadastro-treino/cadastro-treino.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DataBindingComponent,
     LoginComponent,
     MatriculaComponent,
     VerificaAptidaoComponent,
+    RealizaExameAptidaoComponent,
     CadastroTreinoComponent,
-    RealizaExameAptidaoComponent
+
   ],
   imports: [
     BrowserModule,
