879db41405649c67a2dd943c8ecbcda9bdfe79ce
Marcos Junio <innformarcos@gmail.com>
2020-03-23 19:57:09 -0300

    updanting ngmodules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';//Prepara para ser usada no Browser
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
